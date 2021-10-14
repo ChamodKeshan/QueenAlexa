d6628b56d660703f6797649921ec46aadf915228
Lakshitha <69718607+HYPER-MOD@users.noreply.github.com>
2021-10-14 06:07:19 +0000

    File Upload by Hyper Mod

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Queen-Alexa
+<h2 align="center">base Baileys Bot</h2>
+<p align="center">Use at your own risk</p>
+
+## Required
+-   nodejs
+-   git
+
+## Tutorial
+1. Cloning this repository<br />
+```sh
+git clone https://github.com/FERDIZ-afk/base-bot-whatsapp.git
+cd base-bot-whatsapp
+```
+<br />
+
+2. Install dependencies<br />
+```sh
+npm i
+```
+<br />
+
+3. Run Bot<br />
+```sh
+npm start
+```
+<br />
+
+5. untuk session di simpan di folder (/save/session.json)<br />
---
+++ scraper.js
@@ -0,0 +1,75 @@
+
+
+const {
+tiktok,
+tiktokmusic,
+ghstalk,
+telegra,
+yta,
+ytv,
+emoji,
+mediafire,
+covid,
+tahta,
+styleText,
+asmaul,
+fb,
+igstalk,
+twitter,
+playstore,
+tebakgambar,
+kodepos,
+gempa,
+apkmody,
+happymod,
+mcpedl,
+jalantikus,
+moddroid,
+palingmurah,
+kompasnews,
+tribunnews,
+igtv,
+igvideo,
+igfoto,
+halal,
+wallpapper,
+sfilesearch,
+servermc,
+hoax
+} = require('./dl.js');
+
+module.exports.tiktok = tiktok
+module.exports.tiktokmusic = tiktokmusic
+module.exports.ghstalk = ghstalk
+module.exports.telegra = telegra
+module.exports.yta = yta
+module.exports.ytv = ytv
+module.exports.emoji = emoji
+module.exports.mediafire = mediafire
+module.exports.covid = covid
+module.exports.tahta = tahta
+module.exports.styleText = styleText
+module.exports.asmaul = asmaul
+module.exports.fb = fb
+module.exports.igstalk = igstalk
+module.exports.tebakgambar = tebakgambar
+module.exports.playstore = playstore
+module.exports.twitter = twitter
+module.exports.kodepos = kodepos
+module.exports.gempa = gempa
+module.exports.servermc = servermc
+module.exports.happymod = happymod
+module.exports.kompasnews = kompasnews
+module.exports.tribunnews = tribunnews
+module.exports.jalantikus = jalantikus
+module.exports.igtv = igtv
+module.exports.igfoto = igfoto
+module.exports.igvideo = igvideo
+module.exports.hoax = hoax
+module.exports.sfilesearch = sfilesearch
+module.exports.mcpedl = mcpedl
+module.exports.halal =  halal
+module.exports.wallpapper =wallpapper
+module.exports.apkmody = apkmody
+module.exports.moddroid = moddroid
+module.exports.palingmurah = palingmurah
