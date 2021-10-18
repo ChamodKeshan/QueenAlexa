5236acb6bea6faaa8cf8a1b9957e1add7c6697ea
Lakshitha <69718607+HYPER-MOD@users.noreply.github.com>
2021-10-18 06:23:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ const imageToBase64 = require('image-to-base64')
         time_wel = moment.tz("Asia/Jakarta").format("HH:mm");
         
         
-        teks = `Halo ${anu_user} \n\nNama : ${pushname}\nAge :\nGender : Girl/Boy\nAsal :\n\nHope you feel at home and don t forget to fill in\nAnd Following Rules Group`;
+        teks = `Halo ${anu_user} \n\nNama : \nAge :\nGender : Girl/Boy\nAsal :\n\nHope you feel at home and don t forget to fill in\nAnd Following Rules Group`;
         
        buff = await getBuffer(`https://fdz-app.herokuapp.com/api/maker/welcome?name=${anu_user}&picurl=${imge}&namegrp=${encodeURI(mdata.subject)}&imggrp=${ime}&member=@${anu_user}&picbg=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk_nUY-WcxLBvTRJPNBUKhqWA6LdZCmZG75A&usqp=CAU`);
        
